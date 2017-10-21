3437c300a3a349540bde67a46818756d52aa0f97
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-10-22 00:06:33 +0300

    Second Method

---
+++ NET.W.2017.Arkhipenko.2/Seeker/Seeker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Seeker
+{
+    public class Seeker
+    {
+        
+        public static double FindNextBiggerNumber(double number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("The number can't be < 0 ");
+            }
+
+            char[] numberChar = number.ToString().ToCharArray();
+
+           
+            for (int i =(int)number + 1; i < Math.Pow(10,numberChar.Length); i++)
+            {
+                int key = 0;
+                string iString = i.ToString();
+                for (int j = 0; j < numberChar.Length; j++)
+                {
+                    if (iString.Contains(numberChar[j]))
+                    {
+                        key++;
+                        
+                    }
+                }
+                if (key == numberChar.Length)
+                {
+                    return i;
+                }
+            }
+            return -1;
+            
+        }
+    }
+}
