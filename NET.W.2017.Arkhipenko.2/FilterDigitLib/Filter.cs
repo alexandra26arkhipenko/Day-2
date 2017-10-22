35704e53e372257ba44a5601d98fcb5d2970537c
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-10-22 11:44:30 +0300

    Fourth Method

---
+++ NET.W.2017.Arkhipenko.2/FilterDigitLib/Filter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace FilterDigitLib
+{
+    public class Filter
+    {
+        public static ArrayList FilterDigit(int  number, params int[] listInts)
+        {
+            ArrayList listFilter = new ArrayList();
+            for (int i = 0; i < listInts.Length; i++)
+            {
+                string supportString = listInts[i].ToString();
+                string numberString = number.ToString();
+                if (supportString.Contains(numberString))
+                {
+                    listFilter.Add(listInts[i]);
+                }
+            }
+            return listFilter;
+        }
+    }
+}
