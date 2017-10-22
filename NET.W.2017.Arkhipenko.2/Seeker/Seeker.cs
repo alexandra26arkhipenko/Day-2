5764b1fec95c9938bdf35b1949b262b5391a5910
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-10-22 11:13:03 +0300

    Second Method

--- NET.W.2017.Arkhipenko.2/Seeker/Seeker.cs
+++ NET.W.2017.Arkhipenko.2/Seeker/Seeker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Diagnostics;
 
 
 namespace Seeker
@@ -7,16 +8,16 @@ namespace Seeker
     public class Seeker
     {
         
-        public static double FindNextBiggerNumber(double number)
+        public static Tuple<int, TimeSpan > FindNextBiggerNumber(int number)
         {
             if (number < 0)
             {
                 throw new ArgumentException("The number can't be < 0 ");
             }
-
+            Stopwatch stopwatch = new Stopwatch();
             char[] numberChar = number.ToString().ToCharArray();
+            stopwatch.Start();
 
-           
             for (int i =(int)number + 1; i < Math.Pow(10,numberChar.Length); i++)
             {
                 int key = 0;
@@ -26,16 +27,25 @@ namespace Seeker
                     if (iString.Contains(numberChar[j]))
                     {
                         key++;
-                        
+                        var l = iString.IndexOf(numberChar[j]);
+                        iString = iString.Remove(l, 1);
                     }
                 }
                 if (key == numberChar.Length)
                 {
-                    return i;
+                    stopwatch.Stop();
+                    TimeSpan time1 = stopwatch.Elapsed;
+                   
+                    return Tuple.Create(i, time1);
+
                 }
             }
-            return -1;
-            
+
+            stopwatch.Stop();
+            TimeSpan time2 = stopwatch.Elapsed;
+            string time2toString = time2.ToString();
+            return Tuple.Create(-1, time2);
+           
         }
     }
 }
