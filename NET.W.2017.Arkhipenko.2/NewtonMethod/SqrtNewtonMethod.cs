e5ababb1b9a3a5d4cc4431390819765975ceed51
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-10-22 12:14:04 +0300

    Fifth Method

---
+++ NET.W.2017.Arkhipenko.2/NewtonMethod/SqrtNewtonMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NewtonMethod
+{
+    public class SqrtNewtonMethod
+    {
+        public static double FindNthRoot(double number, double n, double eps)
+        {
+            var x0 = number / n;
+            var x1 = (1 / n) * ((n - 1) * x0 + number/ Math.Pow(x0, n - 1));
+
+            while (Math.Abs(x1 - x0) > eps/100)
+            {
+                x0 = x1;
+                x1 = (1 / n) * ((n - 1) * x0 + number / Math.Pow(x0, n - 1));
+            }
+
+            return x1;
+        }
+    }
+}
